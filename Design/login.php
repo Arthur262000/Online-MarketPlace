43172ccc20387bc0a41951cda391a7c9f81f4614
ElyesECE <58076540+ElyesECE@users.noreply.github.com>
2021-03-28 19:43:28 +0200

    debut login

--- Design/login.php
+++ Design/login.php
@@ -1,8 +1,28 @@
 <?php
 
+$mysqli = new mysqli("localhost", "root", "", "projet1");
+$query = "";
+
+if(!$mysqli){
+    echo"Database Connection Error...".mysqli_connect_error();
 
 
-header('Location: index.html');
+
+    
 
 
+} else{
+    echo"<h3>Database Connection Success....</h3>";
+    
+    if($result = mysqli_query($mysqli, "SELECT * from log")){
+        echo mysqli_num_rows($result);  
+        //echo $result;
+
+        $row = mysqli_fetch_array($result);
+        echo $row[0][0];
+    }
+
+}
+
+//header('Location: index.html');
 ?>
