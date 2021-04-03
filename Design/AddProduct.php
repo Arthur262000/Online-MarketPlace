1835346b47dbceafe663b03a051d03fa53fbccd6
Arthur262000 <arthur262000@gmail.com>
2021-04-03 17:38:47 +0200

    Dynamic DropDown AddProduct
    2 dynamic DropdownMenues for Categories and Sell Options

--- Design/AddProduct.php
+++ Design/AddProduct.php
@@ -1,26 +1,78 @@
-<?php
-
-include 'functions.php';
-
-?>
-
 <?=template_header('Add Product')?>
 <link rel="stylesheet" href="bootstrap5\Dropzone\dropzone.css">
 
+    <script language="javascript" type="text/javascript">
+    function Categories(listindex)
+    {
+        document.getElementById("subcategory").length = 0;
+        switch (listindex)
+        {
+        case "Console" :
+            document.getElementById("subcategory").options[0]=new Option("-- Select Sub-Category --","");
+            document.getElementById("subcategory").options[1]=new Option("Playstation","Playstation");
+            document.getElementById("subcategory").options[2]=new Option("Xbox","Xbox");
+            document.getElementById("subcategory").options[3]=new Option("Nintendo","Nintendo");
+            document.getElementById("subcategory").options[4]=new Option("Other","Other");
+            break;
+        case "Video-Game" :
+            document.getElementById("subcategory").options[0]=new Option("-- Select Sub-Category --","");
+            document.getElementById("subcategory").options[1]=new Option("Action","Action");
+            document.getElementById("subcategory").options[2]=new Option("Adventure","Adventure");
+            document.getElementById("subcategory").options[3]=new Option("Battle","Battle");
+            document.getElementById("subcategory").options[4]=new Option("Sport","Sport");
+            break;
+        }
+        return true;
+    }
+    </script>
+
+<script language="javascript" type="text/javascript">
+    function SellOptions(listindex)
+    {
+        document.getElementById("option2").length = 0;
+        switch (listindex)
+        {
+        case "Auction" :
+            document.getElementById("option2").options[0]=new Option("-- Select Option 2 --","");
+            document.getElementById("option2").options[1]=new Option("Buy It Now","BuyItNow");
+            break;
+        case "BuyItNow" :
+            document.getElementById("option2").options[0]=new Option("-- Select Option 2 --","");
+            document.getElementById("option2").options[1]=new Option("Auction","Auction");
+            document.getElementById("option2").options[2]=new Option("Best Offer","BestOffer");
+            break;
+        case "BestOffer" :
+            document.getElementById("option2").options[0]=new Option("-- Select Option 2 --","");
+            document.getElementById("option2").options[1]=new Option("Buy It Now","BuyItNow");
+            break;
+        }
+        return true;
+    }
+    </script>
 <div class="MediumContainer containerItems mt-4 mb-4 pt-4 pb-4">
     <h2 align="center">Add a Product</h2>
     <hr>
     <div class="row">
-        <div class="form-group col-6">
+        <div class="form-group col-4">
             <input type="text" id="Name" name="Name" class="form-control mb-4 mt-4" placeholder="Name"
                                 aria-label="Name"></input>
         </div>
-        <div class="form-group col-6">
-            <select class="form-select mb-4 mt-4" name="Categorie" id="Categorie">
-                <option value="">-Categorie-</option>
+        <div class="form-group col-4">
+            <select class="form-select mb-4 mt-4" class="required-entry" name="Category" id="Category" onchange="javascript: Categories(this.options[this.selectedIndex].value);">
+                <option value="">-- Select Category --</option>
                 <option value="Console">Console</option>
                 <option value="Video-Game">Video Game</option>
             </select>
+        </div>
+        <div class="form-group col-4">
+        <script type="text/javascript" language="JavaScript">
+        document.write('<select class="form-select mb-4 mt-4" name="subcategory" id="subcategory"><option value="">-- Select Sub-Category --</option></select>')
+        </script>
+        <noscript>
+            <select class="form-select mb-4 mt-4" name="subcategory" id="subcategory">
+                <option value="">-- Select Sub-Category --</option>
+            </select>
+        </noscript>
         </div>
     </div>
     <div class="form-row">
@@ -44,16 +96,26 @@ include 'functions.php';
         </div>
     </div>
     <div class="row">
-            <div class="form-group col-6">
+            <div class="form-group col-4">
                     <input type="text" class="form-control mb-3" placeholder="Price" aria-label="Price">
             </div>
-            <div class="form-group col-6">
-                    <select class="form-select mb-3" name="Categorie" id="Categorie">
-                        <option value="">-Option-</option>
+            <div class="form-group col-4">
+                    <select class="form-select mb-3" class="required-entry" name="Categorie" id="Categorie" onchange="javascript: SellOptions(this.options[this.selectedIndex].value);">
+                        <option value="">-- Select Option 1 --</option>
                         <option value="Auction">Auction</option>
                         <option value="BuyItNow">Buy It Now</option>
                         <option value="BestOffer">Best Offer</option>
                     </select>                    
+            </div>
+            <div class="form-group col-4">
+                <script type="text/javascript" language="JavaScript">
+                document.write('<select class="form-select" name="option2" id="option2"><option value="">-- Select Option 2 --</option></select>')
+                </script>
+                <noscript>
+                <select class="form-select" name="option2" id="option2">
+                <option value="">-- Select Option 2 --</option>
+                </select>
+                </noscript>                 
             </div>
     </div>
     <div align='center' class="mb-3 mt-3"> 
--- Design/Payment.php
+++ Design/Payment.php
@@ -1,3 +1,34 @@
 <?=template_header('Payment')?>
 
+<div class="MediumContainer containerItems">
+    <div class="col-6">
+        <div class="mt-4">
+            <h2>Payment Information</h2>
+            <hr> 
+            <div class="col-xs-8">
+               <label type="text" id="CardNum" name="CardNum" class="mb-2 mt-2"
+                    aria-label="Card Number">Card Number</label>
+            </div>
+            <div class="form-row">
+                <div class="col-xs-8">
+                    <label type="text" id="NameDisp" name="NameDisp" class="mb-2 mt-2"
+                        aria-label="NameDisp">Name Displayed</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-4">
+                            <label type="text" class="" id="CVC" name="CVC">CVC</label>
+                        </div>
+                        <div class="form-group col-4">
+                            <label id="Month" name="Month" class="">MM
+                            </label>
+                        </div>
+                        <div class="form-group col-4">
+                            <label type="text" class="mb-4" id="Year" name="Year">YYYY</label>
+                        </div>
+                    </div> 
+        </div>
+    </div>
+</div>
+
 <?=template_footer()?>
